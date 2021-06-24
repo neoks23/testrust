b9e29c427be346f721845431177249f212064af5
neoks23 <57813986+neoks23@users.noreply.github.com>
2021-06-24 23:11:44 +0200

    made sprite move along the x axis

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,16 @@ impl HelloWorld {
 
     #[export]
     fn _ready(&self, _owner: &Node) {
-        godot_print!("hello, world.")
+        godot_print!("hello, world.");
+        let sprite = unsafe { _owner.get_node_as::<Sprite>("Sprite").unwrap() };
+        sprite.set_global_rotation_degrees(45.0);
+    }
+
+    #[export]
+    fn _process(&mut self, _owner: &Node, delta: f32) {
+        let sprite = unsafe { _owner.get_node_as::<Sprite>("Sprite").unwrap() };
+        let position = sprite.global_position() + Vector2::new(1.0, 0.0);
+        sprite.set_global_position(position);
     }
 }
 
